122a418f3d2ded33fe739ced6cc44a2928eb8ff6
md shahid iqbal <111365870+iqbalshaid@users.noreply.github.com>
2024-11-25 10:11:32 +0530

    Update EnglishLoginPage.js

--- App/Components/EnglishLoginPage.js
+++ App/Components/EnglishLoginPage.js
@@ -1,64 +1,48 @@
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, Picker } from 'react-native';
-import React, { useState } from 'react';
+import { View, Text ,Image, TextInput,StyleSheet, TouchableOpacity,Picker} from 'react-native'
+import React,{useState} from 'react'
 import axios from "axios";
-export default function RegisterPage({ navigation }) {
-  const [selectedRole, setSelectedRole] = useState("Business");
-  const [name,setName] = useState('');
-  const [email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
-  const handleRegister =  ()=>{
-    console.log("how are you");
-    //console.log(name);
-    const user = {
-        name:name,
-        email:email,
-    password:password,
-selectedRole:selectedRole}
-   // console.log(user);
-    //post request to the backend
-    try{
-        axios.post("http://10.0.2.2:8000/register",user);
-        Alert.alert("registration successfully");
-    setEmail("");
-    setName("");
-setPassword("");
-setSelectedRole("");
-navigation.navigate("EnglishLoginPage");
 
+export default function EnglishLoginPage({navigation}) {
+  const [selectedRole, setSelectedRole] = useState("Business");
+  const [username,setUserName] = useState('');
+  const [password,setPassword] = useState('');
+  const handleLogin = ()=>{
+    const user = {
+        username:username,
+        password:password,
+        selectedRole:selectedRole
     }
-    catch(err){
-Alert.alert("not registred Error","An Error occured during registration")
-console.log("error in registration",err);
-}
+   // console.log(user);
+    axios.post("http://10.0.2.2:8000/login",user).then((response)=>{
+        //console.log(response);
+        
+       
+      
+
+        // Save the user ID in AsyncStorage or use it as needed
+        
+        navigation.replace("Dashboard");
+    }).catch((err)=>{
+        Alert.alert("Login Error","Invalid Email");
+        console.log(err);
+    });
 }
 
   return (
     <View>
-      <View style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-        <Text style={{ fontSize: 30, marginTop: 50 }}>Register</Text>
+      <View style={{display:"flex", flexDirection:"column", alignItems:"center"}}> 
+        <Image source={require("../Assets/Image/login-page-iml.png")} style={{height:100,width:100,marginTop:70}} ></Image>
+        <Text style={{fontSize:30, display:"flex", flexDirection:"column",marginTop:10}}>Login/SignUp</Text>
       </View>
-      <View style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+      <View style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
         <View>
-          <Text style={{ marginTop: 20 }}>Enter Name</Text>
-          <TextInput placeholder='Name' style={styles.inputForm} 
-          value={name} onChangeText={(text)=>{
-            setName(text)
-          }}/>
-          
-          <Text>Enter Email</Text>
-          <TextInput placeholder='Email' style={styles.inputForm} 
-          value={email}
-          onChangeText={(text)=>{
-            setEmail(text)
-          }}/>
-          
-          <Text>Enter Password</Text>
-          <TextInput placeholder='Password' style={styles.inputForm} secureTextEntry={true} 
-          value={password} onChangeText={(text)=>{
-            setPassword(text)
-          }}/>
-          
-          <Text>Select Role</Text>
+            <Text style={{marginTop:20}}> Enter Username</Text>
+        <TextInput placeholder='UserName' style={styles.inputForn} value={username} onChangeText={(text)=>{
+          setUserName(text)
+        }}/>
+        <Text>Enter Password</Text>
+        <TextInput placeholder='Password' style={styles.inputForn} value={Password} onChangeText={(text)=>setPassword(text)}/ >
+        <Text>Select Role</Text>
           <Picker
             selectedValue={selectedRole}
             style={styles.picker}
@@ -67,56 +51,52 @@ console.log("error in registration",err);
             <Picker.Item label="Business" value="Business" />
             <Picker.Item label="Farmer" value="Farmer" />
           </Picker>
-          
-          <TouchableOpacity
-            style={styles.submitButton}
-            onPress={() => {
-              console.log(`Name, Email, Password, Role: ${selectedRole}`);
-              navigation.navigate("Dashboard");
-            }}
+        <TouchableOpacity style={styles.submitButton} onPress={handleLogin} ><Text style={{color:"white"}}>Submit</Text></TouchableOpacity>
+        <TouchableOpacity
+            onPress={() => navigation.navigate("EnglishRegisterPage")}
+            style={{ marginTop: 10 }}
           >
-            <Text style={{ color: "white" }}>Register</Text>
+            <Text style={{ color: "blue", textAlign: "center" }}>
+              Don't have an account? Register here
+            </Text>
           </TouchableOpacity>
         </View>
+        
+
       </View>
     </View>
-  );
+  )
 }
 
+
 const styles = StyleSheet.create({
-  inputForm: {
-    marginTop: 10,
-    marginBottom: 10,
-    width: 300,
-    backgroundColor: "lightgray",
-    paddingLeft: 40,
-    paddingRight: 40,
-    paddingTop: 10,
-    paddingBottom: 10,
-    borderStyle: "solid",
-    borderColor: "black",
-    borderWidth: 2,
-    borderRadius: 8,
-  },
-  picker: {
-    height: 50,
-    width: 300,
-    backgroundColor: "lightgray",
-    marginVertical: 10,
-    borderColor: "black",
-    borderWidth: 2,
-    borderRadius: 8,
-  },
-  submitButton: {
-    backgroundColor: "black",
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingLeft: 20,
-    paddingRight: 20,
-    borderRadius: 10,
-    marginBottom: 10,
-    marginLeft: 0,
-    width: 100,
-    alignItems: "center",
-  },
-});
+    inputForn:{
+        marginTop:10,
+        marginBottom:10,
+        width:300,
+        backgroundColor:"lightgray",
+        paddingLeft:40,
+        paddingRight:40,
+        paddingTop:10,
+        paddingBottom:10,
+        borderStyle:"solid",
+        borderColor:"black",
+        borderWidth:2,
+        borderRadius:8,
+
+    },
+
+    submitButton:{
+        backgroundColor:"black",
+        paddingTop:10,
+        paddingBottom:10,
+        paddingLeft:20,
+        paddingRight:20,
+        borderRadius:10,
+        marginBottom:10,
+        marginLeft:0,
+        width:100
+    
+    
+      }
+})
